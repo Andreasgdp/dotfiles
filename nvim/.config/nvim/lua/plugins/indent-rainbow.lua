633a39d08d9140c1ed160eec325c247733710742
Andreas Petersen <andreasgdp@gmail.com>
2024-06-10 20:41:42 +0200

    Set indent rainbow to use less vibrant colors in nvim

--- nvim/.config/nvim/lua/plugins/indent-rainbow.lua
+++ nvim/.config/nvim/lua/plugins/indent-rainbow.lua
@@ -39,13 +39,13 @@ return {
     -- create the highlight groups in the highlight setup hook, so they are reset
     -- every time the colorscheme changes
     hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-      vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-      vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-      vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-      vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-      vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-      vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-      vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+      vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#9B4D55" })
+      vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#A5774C" })
+      vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#475976" })
+      vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#9D855F" })
+      vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#6C7E5F" })
+      vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#7E616D" })
+      vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#566F7F" })
     end)
 
     require("ibl").setup({ indent = { highlight = highlight } })
