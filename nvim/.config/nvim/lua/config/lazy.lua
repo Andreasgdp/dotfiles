0a0eb3f315a3eed6ae3c7078ec4ff5c42736e779
Andreas Petersen <andreasgdp@gmail.com>
2024-06-14 23:52:06 +0200

    Ensure good setup for angular from lazyvim config

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -35,6 +35,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.harpoon2" },
     { import = "lazyvim.plugins.extras.coding.luasnip" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
+    { import = "lazyvim.plugins.extras.lang.angular" },
 
     -- import/override with your plugins
     { import = "plugins" },
