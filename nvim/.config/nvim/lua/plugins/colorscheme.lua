f72aa011ce9b1b2c59ed2f4d59a411a6adab58b3
Andreas Petersen <andreasgdp@gmail.com>
2024-06-18 19:02:19 +0200

    High contrast catppuccin mocha theme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,30 @@
 return {
   {
     "catppuccin/nvim",
-    lazy = true,
     name = "catppuccin",
     opts = {
+      term_colors = true,
+      transparent_background = false,
+      styles = {
+        comments = {},
+        conditionals = {},
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+      },
+      color_overrides = {
+        mocha = {
+          base = "#000000",
+          mantle = "#000000",
+          crust = "#000000",
+        },
+      },
       integrations = {
         aerial = true,
         alpha = true,
