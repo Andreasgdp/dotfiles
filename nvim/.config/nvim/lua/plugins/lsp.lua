7d4ddb2b928b33e44810e8f173f87c8e2b0a9774
Andreas Petersen <andreasgdp@gmail.com>
2024-06-18 21:29:18 +0200

    Remove special csharp config, as does not do anything special
    Basically the functionality it brings should already be within the setup
    from lazyvim extras csharp config, which is already setup.

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,17 +1,4 @@
 -- TODO: Add basic LSP support and ensure languages I use have LSP support.
 -- Basically it is only to ensure lsp and mason have the language servers installed
 -- The rest is handled by the setup provided by LazyVim
-return {
-  -- TODO: test this csharp plugin out and see how it fares together with the current csharp setup.
-  -- If it deoes not work well, remove it.
-  "iabdelkareem/csharp.nvim",
-  dependencies = {
-    "williamboman/mason.nvim", -- Required, automatically installs omnisharp
-    "mfussenegger/nvim-dap",
-    "Tastyep/structlog.nvim", -- Optional, but highly recommended for debugging
-  },
-  config = function()
-    require("mason").setup() -- Mason setup must run before csharp
-    require("csharp").setup()
-  end,
-}
+return {}
