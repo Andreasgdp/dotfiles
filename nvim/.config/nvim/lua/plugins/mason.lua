50678430fceb98228d937e37c978a6558c78f956
Andreas Petersen <andreasgdp@gmail.com>
2024-10-02 19:08:24 +0200

    fix(nvim): add java support, update default list for mason and update packages

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,4 +1,23 @@
 return {
   "williamboman/mason.nvim",
-  opts = { ensure_installed = { "angular-language-server" } },
+  opts = {
+    ensure_installed = {
+      "eslint-lsp", -- javascript
+      "prettier", -- javascript
+      "tailwindcss-language-server", -- tailwindcss
+      "typescript-language-server", -- typescript
+      "angular-language-server", -- angular
+      "sonarlint-language-server", -- sonarlint
+      "stylua", -- lua
+      "lua-language-server", -- lua
+      "markdown-toc", -- markdown
+      "markdownlint-cli2", -- markdown
+      "shellcheck", -- shell
+      "shfmt", -- shell
+      "hadolint", -- docker
+      "tflint", -- terraform
+      "omnisharp", -- csharp
+      "jdtls", -- java
+    },
+  },
 }
