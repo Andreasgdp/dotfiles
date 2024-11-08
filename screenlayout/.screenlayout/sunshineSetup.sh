784b66e932fb3b2436d1c596ad0e3ca650c22d95
Andreas Petersen <andreasgdp@gmail.com>
2024-11-08 22:29:58 +0100

    fix: ensure not on vpn when setting up to run sunshine

--- screenlayout/.screenlayout/sunshineSetup.sh
+++ screenlayout/.screenlayout/sunshineSetup.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+nordvpn d
 xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --off --output DP-2 --primary --mode 2560x1440 --pos 0x0 --rotate normal --scale 0.75x0.75 --filter nearest --output DP-3 --off --output DP-4 --off --output DP-5 --off --output USB-C-0 --off
