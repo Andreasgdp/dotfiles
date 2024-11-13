e264659f308d921a8958ecded20258f7b20b0333
Andreas Petersen <andreasgdp@gmail.com>
2024-11-13 09:38:11 +0100

    feat: set file name in bottom right corner
    also include the other properties for easier cofig in future

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -1,5 +1,5 @@
 return {
-  -- filename in top right corner of all file buffers
+  -- filename in bottom right corner of all file buffers
   {
     "b0o/incline.nvim",
     dependencies = {},
@@ -8,9 +8,33 @@ return {
     config = function()
       local helpers = require("incline.helpers")
       require("incline").setup({
-        window = {
-          padding = 0,
-          margin = { horizontal = 1, vertical = 1 },
+        debounce_threshold = {
+          falling = 50,
+          rising = 10,
+        },
+        hide = {
+          cursorline = false,
+          focused_win = false,
+          only_win = false,
+        },
+        highlight = {
+          groups = {
+            InclineNormal = {
+              default = true,
+              group = "NormalFloat",
+            },
+            InclineNormalNC = {
+              default = true,
+              group = "NormalFloat",
+            },
+          },
+        },
+        ignore = {
+          buftypes = "special",
+          filetypes = {},
+          floating_wins = true,
+          unlisted_buffers = true,
+          wintypes = "special",
         },
         render = function(props)
           local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
@@ -25,6 +49,42 @@ return {
           }
           return buffer
         end,
+        window = {
+          margin = {
+            horizontal = 1,
+            vertical = 1,
+          },
+          options = {
+            signcolumn = "no",
+            wrap = false,
+          },
+          overlap = {
+            borders = true,
+            statusline = false,
+            tabline = false,
+            winbar = false,
+          },
+          padding = 1,
+          padding_char = " ",
+          placement = {
+            horizontal = "right",
+            vertical = "bottom",
+          },
+          width = "fit",
+          winhighlight = {
+            active = {
+              EndOfBuffer = "None",
+              Normal = "InclineNormal",
+              Search = "None",
+            },
+            inactive = {
+              EndOfBuffer = "None",
+              Normal = "InclineNormalNC",
+              Search = "None",
+            },
+          },
+          zindex = 50,
+        },
       })
     end,
   },
