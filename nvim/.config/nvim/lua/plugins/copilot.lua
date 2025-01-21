dc57e95e1aee42af7107f750a6171726bc3d2ad7
Andreas Petersen <andreasgdp@gmail.com>
2025-01-21 06:34:04 +0100

    chore(nvim): update and set auto focus sidebar of avante to false

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -59,6 +59,9 @@ return {
             -- required for Windows users
             use_absolute_path = true,
           },
+          behaviour = {
+            auto_focus_sidebar = false,
+          },
         },
       },
       {
