6bd9ad37e8e926fb3ca5d918cd4e8f2725653f35
Andreas Petersen <andreasgdp@gmail.com>
2025-01-29 22:26:30 +0100

    fix(awesome): update akiflow command bar to use brave browser

--- awesome/.config/awesome/launch-files/akiflow-command-bar.sh
+++ awesome/.config/awesome/launch-files/akiflow-command-bar.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 
-# Get window id of akiflow window with name "Akiflow" and class "Google-Chrome"
+# Get window id of akiflow window with name "Akiflow" and class "Brave-browser"
 akiflow_window_id=$(comm -12 \
   <(xdotool search --name  'Akiflow'  | sort) \
-  <(xdotool search --class 'Google-Chrome'  | sort))
+  <(xdotool search --class 'Brave-browser'  | sort))
 
 
 # If window not open, open it with google-chrome-stable --app=https://web.akiflow.com/#/planner/today and make sure to wait for it to load before focusing it and opening command bar
 if [ -z "$akiflow_window_id" ]; then
-    google-chrome-stable --app="https://web.akiflow.com/#/planner/today"
+    brave-browser --app="https://web.akiflow.com/#/planner/today"
     sleep 3
     akiflow_window_id=$(comm -12 \
       <(xdotool search --name  'Akiflow'  | sort) \
-      <(xdotool search --class 'Google-Chrome'  | sort))
+      <(xdotool search --class 'Brave-browser'  | sort))
 fi
 
 # Focus akiflow window
