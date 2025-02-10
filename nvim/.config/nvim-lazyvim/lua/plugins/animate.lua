300f1ed17354be1eab19fe33a0956ee0a11b9fe7
Andreas Petersen <andreasgdp@gmail.com>
2025-02-10 19:35:05 +0100

    feat(nvim+): super fast and smooth scroll

---
+++ nvim/.config/nvim-lazyvim/lua/plugins/animate.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "folke/snacks.nvim",
+    ---@type snacks.Config
+    opts = {
+      scroll = {
+        enabled = true,
+        animate = {
+          duration = { step = 5, total = 50 },
+          easing = "linear",
+        },
+        spamming = 10, -- threshold for spamming detection
+        filter = function(buf)
+          return vim.g.snacks_scroll ~= false
+            and vim.b[buf].snacks_scroll ~= false
+            and vim.bo[buf].buftype ~= "terminal"
+        end,
+      },
+    },
+  },
+  {
+    "sphamba/smear-cursor.nvim",
+
+    opts = { -- Default  Range
+      stiffness = 0.8, -- 0.6      [0, 1]
+      trailing_stiffness = 0.5, -- 0.3      [0, 1]
+      distance_stop_animating = 0.5, -- 0.1      > 0
+    },
+  },
+}
