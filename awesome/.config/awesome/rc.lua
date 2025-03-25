8fd2bd0a16fe09411b0dd61fa5c965419598af69
Andreas Petersen <andreasgdp@gmail.com>
2025-03-25 21:42:30 +0100

    fix(awesome): big fix for focus that should ensure client under cursor is always focused

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -212,6 +212,22 @@ client.connect_signal("mouse::enter", function(c)
     c:emit_signal("request::activate", "mouse_enter", { raise = false })
 end)
 
+tag.connect_signal("property::selected", function(t)
+    local selected = tostring(t.selected) == "false"
+    if selected then
+        local focus_timer = timer({ timeout = 0.05 })
+        focus_timer:connect_signal("timeout", function()
+            local c = awful.mouse.client_under_pointer()
+            if not (c == nil) then
+                client.focus = c
+                c:raise()
+            end
+            focus_timer:stop()
+        end)
+        focus_timer:start()
+    end
+end)
+
 -- ========================================
 -- Misc.
 -- ========================================
