c0dad8575901517be7be0fffd21988638276bcbd
Andreas Petersen <andreas.petersen@jepp.com>
2026-06-24 18:07:54 +0200

    bla

--- hypr/.config/hypr/scripts/start-online-apps.sh
+++ hypr/.config/hypr/scripts/start-online-apps.sh
@@ -7,6 +7,7 @@ elapsed=0
 while [ "$elapsed" -lt "$timeout_seconds" ]; do
     if ping -c 1 -W 1 1.1.1.1 >/dev/null 2>&1; then
         /opt/zen-browser-bin/zen-bin "https://www.youtube.com/" &
+        slack &
         omarchy-launch-webapp "https://web.akiflow.com/#/planner/today" &
         omarchy-launch-webapp "https://www.facebook.com/messages/e2ee/t" &
         omarchy-launch-webapp "https://discord.com/channels/@me" &
